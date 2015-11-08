fd736c66601e1e27bf9e0a9aaa040baee33404bf
shadefinale <shadefinale@gmail.com>
2015-11-07 23:37:17 -0600

    Test solution.

--- solution.js
+++ solution.js
@@ -1,8 +1,10 @@
 Solution = (function() {
   return {
     isRotation: function(str1, str2) {
-      //your solution goes here
-      return 'missing implementation';
-    }    
+      if (str1.length == 0) return false;
+      if (str1.length != str2.length) return false;
+      return (str1 + str1).indexOf(str2) >= 0;
+    }
+
   }
 }());
